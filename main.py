c7e1f19136c3caedc466f8978e9fa59d6852f198
sbs2018 <35073809+ssmolen2018@users.noreply.github.com>
2019-09-13 12:53:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -81,11 +81,13 @@ def workflow(als_max_iter, keras_hidden_units, max_row_limit):
         print("############ START LOAD RAW DATA #############")
         load_raw_data_run = _get_or_run("load_raw_data", {}, git_commit)
         ratings_csv_uri = os.path.join(load_raw_data_run.info.artifact_uri, "ratings-csv-dir/ratings.csv")
-        ratings_csv_data=load_raw_data_run.data.params.get("csv_file")
+        ratings_csv_data=os.path.join("file://",load_raw_data_run.data.params.get("csv_file")
+                                    
         print("############ START ETL DATA #############")
+        print("ratings_csv_data="+ratings_csv_data) 
         print("ratings_csv_uri="+ratings_csv_uri)
         etl_data_run = _get_or_run("etl_data",
-                                   {"ratings_csv": "file://"+ratings_csv_data,
+                                   {"ratings_csv": ratings_csv_data,
                                     "max_row_limit": max_row_limit},
                                    git_commit)
         ratings_parquet_uri = os.path.join(etl_data_run.info.artifact_uri, "ratings-parquet-dir")
