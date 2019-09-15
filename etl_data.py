5b69e76a5ab70a843d7d7d70ac6c949a3c3f78a5
sbs2018 <35073809+ssmolen2018@users.noreply.github.com>
2019-09-14 20:57:23 -0500

    Update etl_data.py

--- etl_data.py
+++ etl_data.py
@@ -14,7 +14,6 @@ import click
 @click.command(help="Given a CSV file (see load_raw_data), transforms it into Parquet "
                     "in an mlflow artifact called 'ratings-parquet-dir'")
 @click.option("--ratings-csv:"+ratings-csv)
-if (ratings-csvstr.startswith( 'this' )")
 @click.option("--max-row-limit", default=10000,
               help="Limit the data size to run comfortably on a laptop.")
 def etl_data(ratings_csv, max_row_limit):
